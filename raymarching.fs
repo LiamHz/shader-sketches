5e67718accfe81fb7bea908c08aaaf90a4dca799
LiamHz <liam.hinzman@gmail.com>
2020-07-06 00:03:03 -0400

    Implemented cylinder SDF and constructive solid geometry operations

--- raymarching.fs
+++ raymarching.fs
@@ -17,10 +17,45 @@ const float MIN_DIST = 0.0;
 const float MAX_DIST = 100.0;
 const float EPSILON = 0.0001;
 
+const float PI = 3.1415926535;
+
 struct Light {
     vec3 position;
     vec3 intensity;
 };
+
+// Affine transforms
+mat4 rotationX(float angle) {
+    float c = cos(angle);
+    float s = sin(angle);
+    return mat4( 1, 0, 0, 0,
+                 0, c,-s, 0,
+                 0, s, c, 0,
+                 0, 0, 0, 1);
+}
+
+mat4 rotationZ(float angle) {
+    float c = cos(angle);
+    float s = sin(angle);
+    return mat4( c,-s, 0, 0,
+                 s, c, 0, 0,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1);
+}
+
+
+// Constructive Solid Geometry (CSG) operations
+float intersectSDF(float distA, float distB) {
+    return max(distA, distB);
+}
+
+float unionSDF(float distA, float distB) {
+    return min(distA, distB);
+}
+
+float differenceSDF(float distA, float distB) {
+    return max(distA, -distB);
+}
 
 // SDF for a cube centered at the origin with width = height = length = 2.0
 float cubeSDF(vec3 p) {
@@ -39,16 +74,69 @@ float cubeSDF(vec3 p) {
     return insideDistance + outsideDistance;
 }
 
-// SDF for a sphere centered at the origin with radius 1
-float sphereSDF(vec3 p, vec3 center) {
-    return length(p - center) - 1.0;
+// SDF for a sphere centered at the origin
+float sphereSDF(vec3 p, float radius) {
+    return length(p) - radius;
+}
+
+// SDF for a cylinder centered at the origin, 
+// a and b are endpoints of the cylinder
+float cylinderSDF(vec3 p, vec3 a, vec3 b, float r) {
+    vec3  ba = b - a;
+    vec3  pa = p - a;
+    float baba = dot(ba,ba);
+    float paba = dot(pa,ba);
+    float x = length(pa*baba-ba*paba) - r*baba;
+    float y = abs(paba-baba*0.5)-baba*0.5;
+    float x2 = x*x;
+    float y2 = y*y*baba;
+    float d = (max(x,y)<0.0)?-min(x2,y2):(((x>0.0)?x2:0.0)+((y>0.0)?y2:0.0));
+    return sign(d)*sqrt(abs(d))/baba;
 }
 
 // Absolute of the return value is distance to surface
 // Sign indicates if point is inside (-) or outside (+) surface
 float sceneSDF(vec3 p) {
-    return cubeSDF(p);
-    return sphereSDF(p, vec3(0.65, 0, 0));
+    // Note: Transformations can be applied by using the inverse 
+    //       of the desired transformation matrix
+    //       EX: p + vec3(-1, 0, 0) will move the SDF 1 unit to the right
+    
+    // Note: Transformations must preserve the distances between points
+    //       EX: sdf(p / scalingFactor) --> will result in distorted distances
+    //           sdf(p / scalingFactor) * scalingFactor --> won't distort
+    
+    // Note: Understimating the distance is better than overestimating
+    //       For non-uniform scaling multiply by the minimum of the components
+    //       EX: sdf(p / vec3(x, y, z)) * min(x, min(y, z))
+    
+    float cubeD   = cubeSDF(p);
+    float sphereD = sphereSDF((p) / 1.2, 1.0) * 1.2;
+    
+    float cS = 1.2;   // Cylinder scaling factor
+    float cR = 0.6 + sin(u_time) * 0.4;
+    float xCylD = cylinderSDF(p, vec3(-1,  0,  0)*cS, vec3(1, 0, 0)*cS, cR);
+    float yCylD = cylinderSDF(p, vec3( 0, -1,  0)*cS, vec3(0, 1, 0)*cS, cR);
+    float zCylD = cylinderSDF(p, vec3( 0,  0, -1)*cS, vec3(0, 0, 1)*cS, cR);
+    
+    float sR = 0.2;                 // Sphere radius
+    float sT = sin(u_time) * 2.0;   // Transform scaler
+    float sUp       = sphereSDF(p - vec3( 0, 1, 0)*sT, sR);
+    float sDown     = sphereSDF(p - vec3( 0,-1, 0)*sT, sR);
+    float sLeft     = sphereSDF(p - vec3(-1, 0, 0)*sT, sR);
+    float sRight    = sphereSDF(p - vec3( 1, 0, 0)*sT, sR);
+    float sForward  = sphereSDF(p - vec3( 0, 0, 1)*sT, sR);
+    float sBack     = sphereSDF(p - vec3( 0, 0,-1)*sT, sR);
+
+    float uniCyls          = unionSDF(xCylD, unionSDF(yCylD, zCylD));
+    float interCircCube    = intersectSDF(cubeD, sphereD);
+    float diffCircCubeCyls = differenceSDF(interCircCube, uniCyls);
+    float sixSpheres       = unionSDF(unionSDF(unionSDF(sUp, sDown),
+                                      unionSDF(sLeft, sRight)),
+                                      unionSDF(sForward, sBack));
+
+    float combo = unionSDF(diffCircCubeCyls, sixSpheres);
+
+    return combo;
 }
 
 // Return shortest distance from the eye to the scene surface along ray 
@@ -109,13 +197,18 @@ vec3 getLightContrib(vec3 p, vec3 eye, vec3 k_d, vec3 k_s, float shininess, Ligh
 }
 
 vec3 getLighting(vec3 p, vec3 eye) {
+    // Use surface normal as ambient color
+    vec3 N = estimateNormal(p);
+    vec3 normalAmbient = (N + vec3(1)) / 2.0;
+    
     vec3 k_a = vec3(0.3);
-    vec3 k_d = vec3(0.7);
+    vec3 k_d = normalAmbient;
     vec3 k_s = vec3(1.0);
     float shininess = 8.0;
 
+
     vec3 ambientLight = vec3(0.2);
-    vec3 lighting = ambientLight * k_a;
+    vec3 lighting = ambientLight * k_a * normalAmbient;
 
     Light light1;
     light1.intensity = vec3(0.4);
@@ -143,7 +236,7 @@ mat4 getLookAtMatrix(vec3 eye, vec3 center, vec3 up) {
 void main() {
     vec2 resolution = u_resolution;
     
-    vec3 eye = vec3(8.0, 5.0, 7.0)/2;
+    vec3 eye = vec3(sin(u_time) * 5.0, 4.0, cos(u_time) * 5.0);
     vec3 viewDir = getRayDirection(45.0, resolution, gl_FragCoord.xy);
 
     // Transform from view to world space
@@ -154,7 +247,7 @@ void main() {
     
     // Didn't hit anything
     if (dist > MAX_DIST - EPSILON) {
-        fragColor = vec4(0.0, 0.0, 0.0, 0.0);
+        fragColor = vec4(0);
         return;
     }
     
@@ -162,7 +255,7 @@ void main() {
     vec3 p = eye + dist * worldDir;
 
     vec3 lighting = getLighting(p, eye);
-    vec3 outColor = vec3(0.8, 0.2, 0.2) * lighting;
+    vec3 outColor = lighting;
 
     fragColor = vec4(outColor, 1);
 }
